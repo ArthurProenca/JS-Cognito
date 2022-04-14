924c61239338fd34048521e8a8d0f6528dd253b3
Arthur Rodrigues Proença <arthur.rodriguesproenca@gmail.com>
2022-04-14 15:37:47 -0300

    Servidor criado.

---
+++ source/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { Application } from 'express';
+
+class App {
+    public app: Application
+    public port: number
+
+    constructor(appInit: { port: number; controllers: any; middleWares: any }) {
+        this.app = express()
+        this.port = appInit.port;
+
+        this.middlewares(appInit.middleWares)
+        this.routes(appInit.controllers)
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+
+    private middlewares(middleWares: any) {
+        middleWares.forEach((middleWare: any) => {
+            this.app.use(middleWare);
+        });
+    }
+
+    private routes(controllers: any) {
+        controllers.forEach((controller : any )=> {
+            this.app.use(controller.path, controller.router);
+        });
+    }
+}
+
+export default App;
